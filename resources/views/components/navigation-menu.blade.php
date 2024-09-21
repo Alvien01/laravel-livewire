9a219ac39e3304a16ead79b3053b0944078bc3d7
Alvien01 <alviantampan0@gmail.com>
2024-09-21 19:09:54 +0700

    sidebar

--- resources/views/components/navigation-menu.blade.php
+++ resources/views/components/navigation-menu.blade.php
@@ -11,9 +11,43 @@
         <x-nav-link :href="route('home')" :active="request()->routeIs('home')">Home</x-nav-link>
         <x-nav-link :href="route('about')" :active="request()->routeIs('about')">About</x-nav-link>
         <x-nav-link :href="route('contact')" :active="request()->routeIs('contact')">Contact</x-nav-link>
-        <!-- <x-nav-link href="/posts">Posts</x-nav-link> -->
         <x-nav-link href="{{ route('post.index') }}" :active="request()->routeIs('post.index')">Posts</x-nav-link>
       </ul>
     </div>
+
+    <!-- Tombol untuk membuka sidebar -->
+    <div class="sidebar">
+  <button class="btn btn-secondary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasSidebar" aria-controls="offcanvasSidebar">
+    <i class="fas fa-bars"></i> Menu
+  </button>
+</div>
+
+
+
   </div>
 </nav>
+
+<!-- Sidebar (Offcanvas) -->
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasSidebar" aria-labelledby="offcanvasSidebarLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasSidebarLabel">Menu</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <x-nav-link :href="route('home')" :active="request()->routeIs('home')">Home</x-nav-link>
+      </li>
+      <li class="nav-item">
+        <x-nav-link :href="route('about')" :active="request()->routeIs('about')">About</x-nav-link>
+      </li>
+      <li class="nav-item">
+        <x-nav-link :href="route('contact')" :active="request()->routeIs('contact')">Contact</x-nav-link>
+      </li>
+      <li class="nav-item">
+        <x-nav-link href="{{ route('post.index') }}" :active="request()->routeIs('post.index')">Posts</x-nav-link>
+      </li>
+    </ul>
+  </div>
+</div>
+
